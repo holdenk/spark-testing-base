058a6494a4bd24aeb036d0729584852b52bbbea1
Holden Karau <holden@pigscanfly.ca>
2015-05-04 21:23:14 -0700

    Start work on providing some base python testing stuff too

---
+++ python/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='spark-testing-base',
+    version='0.0.7-snapshot',
+    author='Holden Karau',
+    author_email='holden@pigscanfly.ca',
+    packages=['spark-testing-base', 'spark-testing-base.test'],
+    url='https://github.com/holdenk/spark-testing-base',
+    license='LICENSE.txt',
+    description='Spark testing for python',
+    long_description=open('../README.md').read(),
+    install_requires=[
+        'unittest2'
+    ],
+    test_requires=[
+        'nose',
+        'coverage',
+        'unittest2'
+    ],
+)
