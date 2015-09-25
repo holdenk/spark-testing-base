b002948f7ca0006c84359a48b7aa8922bf0cfbf9
Holden Karau <holden@pigscanfly.ca>
2015-09-25 14:45:43 -0700

    Start with adding 1.5.0 as a compile target

--- mini-cross-build.pl
+++ mini-cross-build.pl
@@ -2,7 +2,7 @@
 use File::Slurp;
 use strict;
 use warnings;
-my @spark_versions = ("1.3.0", "1.4.0", "1.4.1");
+my @spark_versions = ("1.3.0", "1.4.0", "1.4.1", "1.5.0");
 # Backup the build file
 `cp build.sbt build.sbt_back`;
 # Get the original version
