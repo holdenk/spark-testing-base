a68c71206ed828829b2013a2468ec95c7b5753a5
hanafy <mahmooud.hanafy@gmail.com>
2016-03-12 15:34:42 +0200

    Add binary Dstream testOperation at JavaStreamingSuiteBase

--- src/main/1.3/scala/com/holdenkarau/spark/testing/JavaStreamingSuitebase.scala
+++ src/main/1.3/scala/com/holdenkarau/spark/testing/JavaStreamingSuitebase.scala
@@ -18,7 +18,7 @@ package com.holdenkarau.spark.testing
 
 import java.util.{List => JList}
 
-import org.apache.spark.api.java.function.{Function => JFunction}
+import org.apache.spark.api.java.function.{Function => JFunction, Function2 => JFunction2}
 import org.apache.spark.streaming.api.java._
 import org.apache.spark.streaming.dstream.DStream
 import org.junit.Assert._
@@ -106,11 +106,11 @@ class JavaStreamingSuiteBase extends JavaSuiteBase with StreamingSuiteCommon {
 
     val numBatches = input.size
 
-    implicit val ctagU = ClassTag.AnyRef.asInstanceOf[ClassTag[U]]
-    implicit val ctagV = ClassTag.AnyRef.asInstanceOf[ClassTag[V]]
+    implicit val ctagU = fakeClassTag[U]
+    implicit val ctagV = fakeClassTag[V]
 
-    val sInput = input.map(_.toSeq).toSeq
-    val sExpectedOutput = expectedOutput.map(_.toSeq).toSeq
+    val sInput = toSeq(input)
+    val sExpectedOutput = toSeq(expectedOutput)
 
     def wrappedOperation(input: DStream[U]): DStream[V] = {
       operation.call(new JavaDStream[U](input)).dstream
@@ -122,4 +122,68 @@ class JavaStreamingSuiteBase extends JavaSuiteBase with StreamingSuiteCommon {
       verifyOutput[V](output, sExpectedOutput, ordered)
     }
   }
+
+
+  /**
+    * Test binary DStream operation with two lists of inputs, with number of
+    * batches to run same as the number of input values. The size of the two input lists Should be the same.
+    * You can simulate the input batch as a List of values or as null to simulate empty batch.
+    *
+    * @param input1     First sequence of input collections
+    * @param input2     Second sequence of input collections
+    * @param operation  Binary DStream operation to be applied to the 2 inputs
+    * @param expectedOutput Sequence of expected output collections
+    */
+  def testOperation[U, V, W](input1: JList[JList[U]],
+                             input2: JList[JList[V]],
+                             operation: JFunction2[JavaDStream[U], JavaDStream[V], JavaDStream[W]],
+                             expectedOutput: JList[JList[W]]) {
+    testOperation(input1, input2, operation, expectedOutput, false)
+  }
+
+    /**
+    * Test binary DStream operation with two lists of inputs, with number of
+    * batches to run same as the number of input values. The size of the two input lists Should be the same.
+    * You can simulate the input batch as a List of values or as null to simulate empty batch.
+    *
+    * @param input1     First sequence of input collections
+    * @param input2     Second sequence of input collections
+    * @param operation  Binary DStream operation to be applied to the 2 inputs
+    * @param expectedOutput Sequence of expected output collections
+    * @param ordered Compare output values with expected output values
+    *                within the same output batch ordered or unOrdered.
+    *                Comparing doubles may not work well in case of unordered.
+    */
+  def testOperation[U, V, W](input1: JList[JList[U]],
+                             input2: JList[JList[V]],
+                             operation: JFunction2[JavaDStream[U], JavaDStream[V], JavaDStream[W]],
+                             expectedOutput: JList[JList[W]],
+                             ordered: Boolean) {
+
+    assertEquals("Length of the input lists are not equal", input1.length, input2.length)
+    val numBatches = input1.size
+
+    implicit val ctagU = fakeClassTag[U]
+    implicit val ctagV = fakeClassTag[V]
+    implicit val ctagW = fakeClassTag[W]
+
+    val sInput1 = toSeq(input1)
+    val sInput2 = toSeq(input2)
+    val sExpectedOutput = toSeq(expectedOutput)
+
+    def wrappedOperation(input1: DStream[U], input2: DStream[V]): DStream[W] = {
+      operation.call(new JavaDStream[U](input1), new JavaDStream[V](input2)).dstream
+    }
+
+    withOutputAndStreamingContext(setupStreams[U, V, W](sInput1, sInput2, wrappedOperation)) {
+      (outputStream, ssc) =>
+        val output = runStreams[W](outputStream, ssc, numBatches, expectedOutput.size)
+        verifyOutput[W](output, sExpectedOutput, ordered)
+    }
+  }
+
+  private def toSeq[U](input: JList[JList[U]]) = input.map(_.toSeq).toSeq
+
+  private def fakeClassTag[T]: ClassTag[T] = ClassTag.AnyRef.asInstanceOf[ClassTag[T]]
+
 }
