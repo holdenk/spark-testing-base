e5e5173373f230fe7de64994c079fde53cc5b554
kaatzee <kaatzee@gmail.com>
2016-07-14 00:06:27 +0200

    Make output of check pretty for generated RDD.

--- src/main/1.6/scala/com/holdenkarau/spark/testing/Prettify.scala
+++ src/main/1.6/scala/com/holdenkarau/spark/testing/Prettify.scala
@@ -1,16 +1,25 @@
 package com.holdenkarau.spark.testing
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.DataFrame
 import org.scalacheck.util.Pretty
 
 trait Prettify {
+  val maxNumberOfShownValues = 100
 
-  implicit def prettyDataFrame(dataFrame: DataFrame): Pretty =
-    Pretty { _ => describeDataframe(dataFrame)}
+  implicit def prettyDataFrame(dataframe: DataFrame): Pretty =
+    Pretty { _ => describeDataframe(dataframe)}
+
+  implicit def prettyRDD(rdd: RDD[_]): Pretty =
+    Pretty { _ => describeRDD(rdd)}
 
   private def describeDataframe(dataframe: DataFrame) =
     s"""<DataFrame: schema = ${dataframe.toString}, size = ${dataframe.count()},
-        |values = (${dataframe.take(100).map(_.toString).mkString(", ")})>""".stripMargin.replace("\n", " ")
+        |values = (${dataframe.take(maxNumberOfShownValues).mkString(", ")})>""".stripMargin.replace("\n", " ")
+
+  private def describeRDD(rdd: RDD[_]) =
+    s"""<RDD: size = ${rdd.count()},
+        |values = (${rdd.take(maxNumberOfShownValues).map("\""+_+"\"").mkString(", ")})>""".stripMargin.replace("\n", " ")
 }
 
 object Prettify extends Prettify
--- src/test/1.6/scala/com/holdenkarau/spark/testing/PrettifyTest.scala
+++ src/test/1.6/scala/com/holdenkarau/spark/testing/PrettifyTest.scala
@@ -2,16 +2,17 @@ package com.holdenkarau.spark.testing
 
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
-import org.scalacheck.{Arbitrary, Gen}
+import org.scalacheck.Gen
 import org.scalacheck.Prop._
+import org.scalacheck.util.Pretty
 import org.scalatest.FunSuite
+import org.scalatest.exceptions.GeneratorDrivenPropertyCheckFailedException
 import org.scalatest.prop.Checkers
 
 class PrettifyTest extends FunSuite with SharedSparkContext with Checkers with Prettify {
+  implicit val propertyCheckConfig = PropertyCheckConfig(minSize = 2, maxSize = 2)
 
   test("pretty output of Dataframe's check") {
-    implicit val generatorDrivenConfig =
-      PropertyCheckConfig(minSize = 2, maxSize = 2)
     val schema = StructType(List(StructField("name", StringType), StructField("age", IntegerType)))
     val sqlContext = new SQLContext(sc)
     val nameGenerator = new ColumnGenerator("name", Gen.const("Holden Hanafy"))
@@ -19,35 +20,32 @@ class PrettifyTest extends FunSuite with SharedSparkContext with Checkers with P
 
     val dataframeGen = DataframeGenerator.arbitraryDataFrameWithCustomFields(sqlContext, schema)(nameGenerator, ageGenerator)
 
-    val property =
-      forAll(dataframeGen.arbitrary) {
-        dataframe => false
-      }
-    val e = intercept[org.scalatest.exceptions.GeneratorDrivenPropertyCheckFailedException] {
-      check(property)
-    }
+    val actual = runFailingCheck(dataframeGen.arbitrary)
     val expected =
       Some("arg0 = <DataFrame: schema = [name: string, age: int], size = 2, values = ([Holden Hanafy,20], [Holden Hanafy,20])>")
-    assert(takeSecondToLastLine(e.message) == expected)
+    assert(actual == expected)
   }
 
   test("pretty output of RDD's check") {
-    implicit val generatorDrivenConfig =
-      PropertyCheckConfig(minSize = 2, maxSize = 2)
-    val property =
-      forAll(RDDGenerator.genRDD[String](sc)(Arbitrary.arbitrary[String])) {
-        rdd => {
-          println(rdd.take(10).mkString(","))
-          println(rdd.getNumPartitions)
-          false
-        }
-      }
-    val e = intercept[org.scalatest.exceptions.GeneratorDrivenPropertyCheckFailedException] {
+    val rddGen = RDDGenerator.genRDD[(String, Int)](sc) {
+      for {
+        name <- Gen.const("Holden Hanafy")
+        age <- Gen.const(20)
+      } yield name -> age
+    }
+
+    val actual = runFailingCheck(rddGen)
+    val expected =
+      Some("""arg0 = <RDD: size = 2, values = ("(Holden Hanafy,20)", "(Holden Hanafy,20)")>""")
+    assert(actual == expected)
+  }
+
+  private def runFailingCheck[T](genUnderTest: Gen[T])(implicit p: T => Pretty) = {
+    val property = forAll(genUnderTest)(_ => false)
+    val e = intercept[GeneratorDrivenPropertyCheckFailedException] {
       check(property)
     }
-    val expected =
-      Some("arg0 = <DataFrame: schema = [name: string, age: int], size = 2, values = ([Holden Hanafy,20], [Holden Hanafy,20])>")
-    assert(takeSecondToLastLine(e.message) == expected)
+    takeSecondToLastLine(e.message)
   }
 
   private def takeSecondToLastLine(msg: Option[String]) =
