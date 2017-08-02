262273013e1e95a47661e2c825638a7064522435
Holden Karau <holden@us.ibm.com>
2017-08-02 00:08:06 -0700

    Use reflection internals in pre-2.2 land instead

--- src/main/pre-2.2/scala/org/apache/spark/EvilSparkContext.scala
+++ src/main/pre-2.2/scala/org/apache/spark/EvilSparkContext.scala
@@ -1,12 +1,17 @@
 package org.apache.spark
 
+import java.util.concurrent.atomic.AtomicReference
+
 /**
  * Access Spark internals.
  */
 object EvilSparkContext {
   def stopActiveSparkContext(): Unit = {
-    // This is very slow and creates a SparkContext if one doesn't exist
-    // it is less than ideal but the active context is hidden from us in 2.X
-    SparkContext.getOrCreate().stop()
+    val declaredFields = classOf[SparkContext$].getDeclaredFields()
+    declaredFields.foreach{field => field.setAccessible(true) }
+    val activeContextField =  declaredFields.filter(_.getName.contains("active"))
+    val activeContextValue = activeContextField.map(field => field.get(SparkContext$.MODULE$))
+    val activeContextRef = activeContextValue.filter(ctx => ctx != null && ctx.isInstanceOf[AtomicReference[_]])
+    activeContextRef.foreach(ctx => Option(ctx.asInstanceOf[AtomicReference[SparkContext]].get()).foreach(_.stop()))
   }
 }
