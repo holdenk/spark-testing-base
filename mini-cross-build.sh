30c5597125dab286fa8474ee8e475e4d5980a62e
Holden Karau <holden@pigscanfly.ca>
2023-10-08 22:47:05 -0700

    Bump to 1.4.5

--- mini-cross-build.sh
+++ mini-cross-build.sh
@@ -6,6 +6,7 @@ for spark_version in 2.4.8 3.0.0 3.0.1 3.0.2 3.1.1 3.1.2 3.1.3 3.2.0 3.2.1 3.2.2
     mkdir -p "${build_dir}"
     cp -af ./ "${build_dir}"
     cd ${build_dir}
+    sleep 2
     sbt  -DsparkVersion=$spark_version version clean +publishLocal +publishSigned &
     cd -
 done
