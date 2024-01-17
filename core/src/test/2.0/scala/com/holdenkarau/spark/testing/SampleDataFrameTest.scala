054123106f1197009809b4373bdeb84f3c26fec2
Eduardo Ruiz <eduardo.ruiz@hablapps.com>
2024-01-17 17:45:38 +0100

    fix: [~] #404 PR#213 BigDecimal tolerance

--- core/src/test/2.0/scala/com/holdenkarau/spark/testing/SampleDataFrameTest.scala
+++ core/src/test/2.0/scala/com/holdenkarau/spark/testing/SampleDataFrameTest.scala
@@ -121,6 +121,10 @@ class SampleDataFrameTest extends ScalaDataFrameSuiteBase {
     val row10 = Row(Timestamp.valueOf("2018-01-12 20:23:13"))
     val row11 = Row("abc", 1.1, Timestamp.valueOf("2018-01-12 20:23:13"))
     val row11a = Row("abc", 1.2, Timestamp.valueOf("2018-01-12 20:23:15"))
+    val row12 = Row(new java.math.BigDecimal(1.0))
+    val row12a = Row(new java.math.BigDecimal(1.0 + 1.0E-6))
+    val row13 = Row(scala.math.BigDecimal(1.0))
+    val row13a = Row(scala.math.BigDecimal(1.0 + 1.0E-6))
     assert(false === approxEquals(row, row2, 1E-7, Duration.ZERO))
     assert(true === approxEquals(row, row2, 1E-5, Duration.ZERO))
     assert(true === approxEquals(row3, row3, 1E-5, Duration.ZERO))
@@ -136,6 +140,8 @@ class SampleDataFrameTest extends ScalaDataFrameSuiteBase {
     assert(true === approxEquals(row9, row10, 0, Duration.ofSeconds(60)))
     assert(false === approxEquals(row9, row10, 0, Duration.ofSeconds(53)))
     assert(true === approxEquals(row11, row11a, 0.1, Duration.ofSeconds(5)))
+    assert(true === approxEquals(row12, row12a, 1.0E-6, Duration.ZERO))
+    assert(true === approxEquals(row13, row13a, 1.0E-6, Duration.ZERO))
   }
 
   test("verify hive function support") {
