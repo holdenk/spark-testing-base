e59b1e149faf142bc8a67045ce177d73f0ca41d4
Holden Karau <holden@pigscanfly.ca>
2024-09-30 13:12:52 -0700

    Restore parallel run.

--- mini-cross-build.sh
+++ mini-cross-build.sh
@@ -7,7 +7,7 @@ for spark_version in 2.4.8 3.0.0 3.0.1 3.0.2 3.1.1 3.1.2 3.1.3 3.2.0 3.2.1 3.2.2
     cp -af ./ "${build_dir}"
     cd ${build_dir}
     sleep 2
-    sbt  -DsparkVersion=$spark_version version clean +publishSigned sonatypeBundleRelease
+    sbt  -DsparkVersion=$spark_version version clean +publishSigned sonatypeBundleRelease &
     cd -
 done
 wait
