b1bc842c698c88066f7c420ebbd4a68d982f33c9
Holden Karau <holden@pigscanfly.ca>
2025-09-01 14:57:32 -0700

    Fix

--- core/src/main/4.0/scala/org/apache/spark/sql/internal/evilTools.scala
+++ core/src/main/4.0/scala/org/apache/spark/sql/internal/evilTools.scala
@@ -1,8 +1,11 @@
 package org.apache.spark.sql.internal
 
 import org.apache.spark.sql._
+// Switched between 4 preview and 4
+import org.apache.spark.sql.classic._
 import org.apache.spark.sql.internal._
 import org.apache.spark.sql.catalyst.expressions._
+
 
 object EvilExpressionColumnNode {
   def getExpr(node: ColumnNode): Expression = {
