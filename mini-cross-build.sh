6759f26b74da49b7812240c94df860a52973bd8c
Holden Karau <holden@pigscanfly.ca>
2025-09-01 13:35:51 -0700

    Use nice to allow the system to remain usable during a build :p

--- mini-cross-build.sh
+++ mini-cross-build.sh
@@ -8,7 +8,7 @@ for spark_version in 2.4.8 3.0.0 3.0.1 3.0.2 3.1.1 3.1.2 3.1.3 3.2.0 3.2.1 3.2.2
       cp -af ./ "${build_dir}"
       cd ${build_dir}
       sleep 2
-      sbt  -DsparkVersion=$spark_version version clean +publishSigned sonaUpload &
+      nice -n 10 sbt  -DsparkVersion=$spark_version version clean +publishSigned sonaUpload &
       cd -
 done
 wait
