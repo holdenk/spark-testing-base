7d4de65fee1156009183a4663036b137189a15bc
Holden Karau <holden@pigscanfly.ca>
2025-10-26 12:58:05 -0700

    Fix usage of JAVA_HOME so I can still publish 2.4.8 easily

--- mini-cross-build.sh
+++ mini-cross-build.sh
@@ -10,11 +10,16 @@ spark_versions=(
   3.5.0 3.5.1 3.5.2 3.5.3 3.5.4 3.5.6
   4.0.0 4.0.1
 )
-# Avoid m2 race conditions which "shouldn't" happen but do :p
+# sbt doesn't read the JAVA_HOME env variable.
+SBT_EXTRA=""
+if [ ! -z "$JAVA_HOME" ]; then
+  SBT_EXTRA="-java-home $JAVA_HOME"
+fi
+# Avoid m2 race conditions which "shouldn't" happen but do by running update in serial.
 for spark_version in "${spark_versions[@]}"
 do
   echo "Updating $spark_version"
-  sbt -DsparkVersion=$spark_version +update
+  sbt $SBT_EXTRA -DsparkVersion=$spark_version +update
 done
 for spark_version in "${spark_versions[@]}"
     do
@@ -25,7 +30,7 @@ for spark_version in "${spark_versions[@]}"
       cp -af ./ "${build_dir}"
       cd ${build_dir}
       sleep 2
-      nice -n 10 sbt  -DsparkVersion=$spark_version version clean +publishSigned sonaUpload &
+      nice -n 10 sbt  $SBT_EXTRA -DsparkVersion=$spark_version version clean +publishSigned sonaUpload &
       cd -
 done
 wait
