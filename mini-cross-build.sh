c81aefedd15dd9ed882c149590b7389105e24dea
Holden Karau <holden@pigscanfly.ca>
2025-10-26 12:54:05 -0700

    4.0.1 to mini build

--- mini-cross-build.sh
+++ mini-cross-build.sh
@@ -8,7 +8,7 @@ spark_versions=(
   3.3.0 3.3.1 3.3.2
   3.4.0 3.4.1 3.4.2
   3.5.0 3.5.1 3.5.2 3.5.3 3.5.4 3.5.6
-  4.0.0
+  4.0.0 4.0.1
 )
 # Avoid m2 race conditions which "shouldn't" happen but do :p
 for spark_version in "${spark_versions[@]}"
