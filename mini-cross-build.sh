e003139133ed77c6f8f11f52f04cbb71031e07e9
Holden Karau <holden@pigscanfly.ca>
2026-03-20 23:49:05 -0700

    4.2.0-preview3

--- mini-cross-build.sh
+++ mini-cross-build.sh
@@ -9,7 +9,8 @@ spark_versions=(
   3.4.0 3.4.1 3.4.2
   3.5.0 3.5.1 3.5.2 3.5.3 3.5.4 3.5.6
   4.0.0 4.0.1
-  4.1.0
+  4.1.0 4.1.1
+  4.2.0-preview3
 )
 # sbt doesn't read the JAVA_HOME env variable.
 SBT_EXTRA=""
